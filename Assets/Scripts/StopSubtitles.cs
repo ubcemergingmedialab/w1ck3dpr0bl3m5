f7c2971e018ed29616f8403b7da77253413cfbf5
MichaelXian <michaelkxian@gmail.com>
2018-01-22 22:37:36 -0800

    Fixed: glass/fader, transitioning, subtitles/audio (will add fix to the rest of the transition buttons later)

---
+++ Assets/Scripts/StopSubtitles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using Narrate;
+
+public class StopSubtitles : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void stopSubtitles(){
+		SubtitleManager subMan = SubtitleManager.FindObjectOfType<SubtitleManager>();
+		NarrationManager narMan = NarrationManager.FindObjectOfType<NarrationManager>();
+		subMan.Stop();
+		narMan.GetComponent<AudioSource>().Stop();
+	}
+}
--- Assets/Scripts/TransitionTo.cs
+++ Assets/Scripts/TransitionTo.cs
@@ -4,37 +4,46 @@ using UnityEngine;
 
 public class TransitionTo : MonoBehaviour {
 	public GameObject target;
-    public GameObject hide;
-	bool transitioning;
+	public static bool TRANSITIONTO_TRANSITIONING;
 	Animator fader;
+	GameObject glass;
+
 
 	// Use this for initialization
 	void Start () {
 		if (target == null) {
 			target = GetComponent<GameObject>(); //Gets the sphere that you're transitioning to
         }
-		transitioning = false; //This keeps the transitionTo from being run while already transitioning
+		TRANSITIONTO_TRANSITIONING = false; //This keeps the transitionTo from being run while already transitioning
 		fader = GameObject.Find("Fader").GetComponent<Animator>(); //The animator of the black rectangle that makes the fade
-        hide = GameObject.Find("Glass"); //Gets the glass, which is assumed to be the last object in the hierarchy
+        glass = GameObject.Find("Glass"); //Gets the glass, which is assumed to be the last object in the hierarchy
     }
 
 
 	public void doTransition(){
-		if (!transitioning){
-			transitioning = true;
+		if (!TRANSITIONTO_TRANSITIONING){
+			TRANSITIONTO_TRANSITIONING = true;
+
 			fader.SetTrigger("Fader In");
+			foreach (Animator a in (glass.GetComponentsInChildren<Animator>())) {
+				a.SetTrigger("Fader Out");
+			}
 			Invoke("createNewSphere", 2);
 			Invoke("destroySphere", 2);
 			Invoke("fadeOut", 2);
-			hide.SetActive(true);
+			//hide.SetActive(false);
 		}
 	}
 
 	void fadeOut(){
 		fader.SetTrigger("Fader Out");
+		foreach (Animator a in (glass.GetComponentsInChildren<Animator>())) {
+			a.SetTrigger("Fader In");
+		}
 	}
 	void createNewSphere(){
 		GameObject nextPhotoSphere = (GameObject)Instantiate(target); //This var isn't used but not declaring it makes it not work
+		//hide.SetActive(true);
 	}
 
 	void destroySphere(){
