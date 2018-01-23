c2e3846cda4c0eb85f7cabac49858a207442885c
MichaelXian <michaelkxian@gmail.com>
2018-01-23 02:09:35 -0800

    Perfect menu button placements with fancy math (i.e. trig so not so fancy)

---
+++ Assets/Scripts/FancyTrigPlacement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FancyTrigPlacement : MonoBehaviour {
+	private int total;
+	private GameObject menu;
+	private Transform[] transformList;
+	private float x;
+	private float z;
+	private float radius;
+	// Use this for initialization
+	void Start () {
+		menu = GameObject.Find("Menu");
+		transformList = menu.GetComponentsInChildren<Transform>();
+		total = transformList.Length;
+		radius = 57.8f;
+		// for whatever weird reason unity gets the parent when you call GetComponentsInChildren, so i starts at 1, and total is total-1 
+		for (int i = 1; i < total; i++) {
+			// -1 to flip direction
+			float theta = (float) (- 1 * (i + 4.5) * (2 * Mathf.PI) / (total - 1));
+			x = radius * Mathf.Cos(theta);
+			z = radius * Mathf.Sin(theta);
+			transformList[i].position = new Vector3(x, 0, z);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
