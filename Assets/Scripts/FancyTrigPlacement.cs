672e3abe9e4048fd4d2ad67f907d1f885428bfda
MichaelXian <michaelkxian@gmail.com>
2018-01-25 16:41:34 -0800

    Added starting problem in menu (which problem you face on initialization)

--- Assets/Scripts/FancyTrigPlacement.cs
+++ Assets/Scripts/FancyTrigPlacement.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FancyTrigPlacement : MonoBehaviour {
+	public int startProblem;
+
 	private int total;
 	private GameObject menu;
 	private Transform[] transformList;
@@ -10,19 +12,22 @@ public class FancyTrigPlacement : MonoBehaviour {
 	private float z;
 	private float radius;
 	private bool done;
+	private List<int> problemList = new List<int> {1,2,4,5};
+
 	// Use this for initialization
 	void Start () {
+		startProblem = problemList.IndexOf(startProblem);
 		menu = GameObject.Find("Menu");
 		if (menu == null) {
 			menu = GameObject.Find("Menu(Clone)");
 		}
 		transformList = menu.GetComponentsInChildren<Transform>();
 		total = transformList.Length;
-		radius = 57.8f;
+		radius = 56.8f;
 		// for whatever weird reason unity gets the parent when you call GetComponentsInChildren, so i starts at 1, and total is total-1 
 		for (int i = 1; i < total; i++) {
 			// -1 to flip direction
-			float theta = (float) (- 1 * (i + 4.5) * (2 * Mathf.PI) / (total - 1));
+			float theta = (float) (- 1 * (i + 2 * startProblem + 0.5) * (2 * Mathf.PI) / (total - 1) + Mathf.PI);
 			x = radius * Mathf.Cos(theta);
 			z = radius * Mathf.Sin(theta);
 			transformList[i].position = new Vector3(x, 0, z);
