af4dcf6f100d16d163312e67797fb12b0727aa48
MichaelXian <michaelkxian@gmail.com>
2018-01-29 19:08:11 -0800

    Fixed interview not re-playing; Added interview prefab

---
+++ Assets/Scripts/ReplaceInterview.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaceInterview : MonoBehaviour {
+
+
+	public void replace(){
+		GameObject nextInterview = (GameObject)Instantiate(gameObject);
+		GameObject sphere = gameObject.transform.parent.gameObject;
+		nextInterview.transform.position = nextInterview.transform.position * 100;
+		nextInterview.transform.parent = sphere.transform;
+		nextInterview.transform.localScale = nextInterview.transform.localScale * 100;
+		GameObject.Destroy(gameObject);
+	}
+		
+}
